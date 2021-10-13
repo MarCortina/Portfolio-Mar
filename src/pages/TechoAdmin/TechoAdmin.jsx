cf04885cd6a418a18944340028c7c6737e704bb6
MarCortina <marmica24@hotmail.com>
2021-10-13 12:17:05 -0300

    update techo admin

--- src/pages/TechoAdmin/TechoAdmin.jsx
+++ src/pages/TechoAdmin/TechoAdmin.jsx
@@ -1,13 +1,27 @@
-import React from "react"
-import Styles from "./TechoAdmin.module.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import Styles from "./TechoAdmin.module.css";
+import Window from "../../components/Windows/Window";
+import TechoAdminVideo from "../../assets/TechoAdmin.mp4";
 
 const TechoAdmin = () => {
-    return (
-        <div>
-            <h3>TechoAdmin</h3>
-            aca va solo el video y description del proyecto
-        </div>
-    )
-}
+  return (
+    <div>
+      <h3>TechoAdmin</h3>
+      <Window>
+        <video
+          className={Styles.video}
+          src={TechoAdminVideo}
+          autoPlay
+          loop
+          muted
+        ></video>
+        <Link className={Styles.linkVideo} to="/">
+          <span className={Styles.linkVideo}>see details</span>
+        </Link>
+      </Window>
+    </div>
+  );
+};
 
-export default TechoAdmin
+export default TechoAdmin;
