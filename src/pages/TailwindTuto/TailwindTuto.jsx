d3b1cb70763fe368e7dab58442892946b4317408
MarCortina <marmica24@hotmail.com>
2021-10-13 11:40:37 -0300

    update tailwind tuto

--- src/pages/TailwindTuto/TailwindTuto.jsx
+++ src/pages/TailwindTuto/TailwindTuto.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Styles from "./TailwindTuto.module.css";
+import Window from "../../components/Windows/Window";
+
+import TailwindVideo from "../../assets/Tailwind.mp4";
 
 const TailwindTuto = () => {
   return (
     <div>
       <h3>Tailwind Tuto</h3>
-      screen link y description
+      <Window>
+        <video
+          className={Styles.video}
+          src={TailwindVideo}
+          autoPlay
+          loop
+        ></video>
+        {/* <Link to="/"> */}
+        <a
+          className={Styles.linkVideo}
+          href="https://mar-next-tail.vercel.app/"
+        >
+          <span className={Styles.linkVideo}>visit the web</span>
+        </a>
+        {/* </Link> */}
+      </Window>
     </div>
   );
 };
