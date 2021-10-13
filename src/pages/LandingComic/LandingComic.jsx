254d2912239b54ff5ba995d01a48b22198548524
MarCortina <marmica24@hotmail.com>
2021-10-13 11:30:31 -0300

    landing update video & styles

--- src/pages/LandingComic/LandingComic.jsx
+++ src/pages/LandingComic/LandingComic.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Styles from './LandingComic.module.css';
+import React from "react";
+import Window from "../../components/Windows/Window";
+import LandingVideo from "../../assets/Landing.mp4";
+import { Link } from "react-router-dom";
+import Styles from "./LandingComic.module.css";
 
 const LandingComic = () => {
-    return (
-        <div>
-            <h3>Landing Comic</h3>
-            captura link y description
-        </div>
-    )
-}
+  return (
+    <div>
+      <h3>Landing Comic</h3>
+      <Window>
+        <video 
+            className={Styles.video}
+            autoPlay
+            loop
+            muted
+            playsInline
+            src={LandingVideo}
+        />
+         {/* <Link to="/"> */}
+         <a className={Styles.linkVideo} href="https://landing-comic.vercel.app/">
+          <span className={Styles.linkVideo}>visit the web</span>
+        </a>
+        {/* </Link> */}
+      </Window>
+    </div>
+  );
+};
 
-export default LandingComic
+export default LandingComic;
